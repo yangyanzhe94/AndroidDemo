b8668ada110bda23cd8ddefd91cc7745f14d3466
yanzhe.yang <yanzhe.yang@ihandysoft.com>
2019-03-26 10:50:54 +0800

    feat(*): add

--- app/src/main/java/com/yyz/androiddemo/MainActivity.java
+++ app/src/main/java/com/yyz/androiddemo/MainActivity.java
@@ -3,7 +3,6 @@ package com.yyz.androiddemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.yyz.feast.androiddemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
